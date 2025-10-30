c656b2194cca9fa3cdd73eafdd642c7ed1b3f0ab
sbeinart86 <61475834+sbeinart86@users.noreply.github.com>
2025-10-30 14:11:49 -0400

    Add files via upload
    Fixed order to PF, ESPT, Outpost

--- script.bat
+++ script.bat
@@ -6,55 +6,55 @@ echo Launching Seed...
 echo.
 echo Checking Player counts ..
 
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.player_count_by_team.allied"`) do set alliedcountHOT=%%i
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.player_count_by_team.axis"`) do set axiscountHOT=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.player_count_by_team.allied"`) do set alliedcountPATH=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.player_count_by_team.axis"`) do set axiscountPATH=%%i
 
-IF NOT DEFINED axiscountHOT goto ServerDownHOT
-IF DEFINED axiscountHOT goto ServerUpHOT
-:ServerDownHOT
+IF NOT DEFINED axiscountPATH goto ServerDownPATH
+IF DEFINED axiscountPATH goto ServerUpPATH
+:ServerDownPATH
 echo Server is Down. Skipping to next Server
 goto ESPTSEED
-:ServerUpHOT
-echo.Allied Faction has %alliedcountHOT% players
-echo.Axis Faction has %axiscountHOT% players
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.player_count"`) do set countHOT=%%i
-echo.Player Count %countHOT%
-If %countHOT% gtr %SEEDED_HOT% (
+:ServerUpPATH
+echo.Allied Faction has %alliedcountPATH% players
+echo.Axis Faction has %axiscountPATH% players
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.player_count"`) do set countPATH=%%i
+echo.Player Count %countPATH%
+If %countPATH% gtr %SEEDED_PATH% (
 goto ESPTSEED
 )
 
-if %alliedcountHOT% leq %axiscountHOT% (
+if %alliedcountPATH% leq %axiscountPATH% (
 echo Launching as Allies. Time to Launch 4.5 Minutes.
-SpawnSL.exe Allied %SERVER_NAMEHOT%
+SpawnSL.exe Allied %SERVER_NAMEPATH%
 timeout /t 10 >nul
-goto HOTloop
+goto PATHloop
 ) else (
 echo Launching as Axis. Time to Launch 4.5 Minutes.
-SpawnSL.exe Axis %SERVER_NAMEHOT%
+SpawnSL.exe Axis %SERVER_NAMEPATH%
 timeout /t 10 >nul
 
-goto HOTloop
+goto PATHloop
 )
 
 
 
-:HOTloop
+:PATHloop
 
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.player_count"`) do set countHOT=%%i
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.time_remaining"`) do set timeHOT=%%i
-for /f "tokens=1,2 delims=." %%a  in ("%timeHOT%") do (set timeHOT=%%a)
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.player_count_by_team.allied"`) do set alliedcountHOT=%%i
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.player_count_by_team.axis"`) do set axiscountHOT=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.player_count"`) do set countPATH=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.time_remaining"`) do set timePATH=%%i
+for /f "tokens=1,2 delims=." %%a  in ("%timePATH%") do (set timePATH=%%a)
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.player_count_by_team.allied"`) do set alliedcountPATH=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.player_count_by_team.axis"`) do set axiscountPATH=%%i
 
-if %countHOT% gtr %SEEDED_HOT% (
-    echo Player count is greater than %SEEDED_HOT%.
+if %countPATH% gtr %SEEDED_PATH% (
+    echo Player count is greater than %SEEDED_PATH%.
     goto endloop
 ) else (
-    echo Player count is %countHOT%. Waiting 30 seconds...
-	echo Timeleft: %timeHOT%
-	if %timeHOT% geq 5280 (
+    echo Player count is %countPATH%. Waiting 30 seconds...
+	echo Timeleft: %timePATH%
+	if %timePATH% geq 5280 (
 	echo New Map.
-		if %alliedcountHOT% leq %axiscountHOT% (
+		if %alliedcountPATH% leq %axiscountPATH% (
 		echo Spawning
 		ReSpawnSL.exe Allied
 		) else (
@@ -62,10 +62,10 @@ if %countHOT% gtr %SEEDED_HOT% (
 		ReSpawnSL.exe Axis
 		)
 	timeout /t 120 >nul
-	goto HOTloop
+	goto PATHloop
 	) else (
     timeout /t 30 >nul
-    goto HOTloop
+    goto PATHloop
 )
 )
 
@@ -85,15 +85,15 @@ for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLESPT% ^| %JQ_PATH% -
 IF NOT DEFINED axiscountESPT goto ServerDownESPT
 IF DEFINED axiscountESPT goto ServerUpESPT
 :ServerDownESPT
-echo Server is Down. Skipping to Pathfinders.
-goto PathSeed
+echo Server is Down. Skipping to Outpost.
+goto HOTSeed
 :ServerUpESPT
 echo.Allied Faction has %alliedcountESPT% players
 echo.Axis Faction has %axiscountESPT% players
 for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLESPT% ^| %JQ_PATH% -r ".result.player_count"`) do set countESPT=%%i
 echo.Player Count %countESPT%
 If %countESPT% gtr %SEEDED_ESPT% (
-goto PathSEED
+goto PathHOT
 )
 
 if %alliedcountESPT% leq %axiscountESPT% (
@@ -147,61 +147,61 @@ if %countESPT% gtr %SEEDED_ESPT% (
 altf4.exe
 echo Waiting for HLL to Close.
 timeout /t 60 >nul
-:PathSEED
-echo Server is seeded. Onto Pathfinders
+:HOTSEED
+echo Server is seeded. Onto Outpost
 echo Launching Seed...
 echo.
 echo Checking Player counts ..
 
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.player_count_by_team.allied"`) do set alliedcountPATH=%%i
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.player_count_by_team.axis"`) do set axiscountPATH=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.player_count_by_team.allied"`) do set alliedcountHOT=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.player_count_by_team.axis"`) do set axiscountHOT=%%i
 
-IF NOT DEFINED axiscountPATH goto ServerDownPATH
-IF DEFINED axiscountPATH goto ServerUpPATH
-:ServerDownPATH
+IF NOT DEFINED axiscountHOT goto ServerDownHOT
+IF DEFINED axiscountHOT goto ServerUpHOT
+:ServerDownHOT
 echo Server is Down. Skipping to end.
 goto endloop
-:ServerUpPATH
-echo.Allied Faction has %alliedcountPATH% players
-echo.Axis Faction has %axiscountPATH% players
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.player_count"`) do set countPATH=%%i
-echo.Player Count %countPATH%
-If %countPATH% gtr %SEEDED_PATH% (
+:ServerUpHOT
+echo.Allied Faction has %alliedcountHOT% players
+echo.Axis Faction has %axiscountHOT% players
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.player_count"`) do set countHOT=%%i
+echo.Player Count %countHOT%
+If %countHOT% gtr %SEEDED_HOT% (
 goto endloop
 )
 
-If %alliedcountPATH% leq %axiscountPATH% (
+If %alliedcountHOT% leq %axiscountHOT% (
 echo Launching as Allies. Time to Launch 4.5 Minutes.
-SpawnSL.exe Allied %SERVER_NAMEPATH%
+SpawnSL.exe Allied %SERVER_NAMEHOT%
 timeout /t 10 >nul
-goto PATHloop
+goto HOTloop
 ) else (
 echo Launching as Axis. Time to Launch 4.5 Minutes.
-SpawnSL.exe Axis %SERVER_NAMEPATH%
+SpawnSL.exe Axis %SERVER_NAMEHOT%
 timeout /t 10 >nul
 
-goto PATHloop
+goto HOTloop
 )
 
 
 
-:PATHloop
+:HOTloop
 
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.player_count"`) do set countPATH=%%i
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.time_remaining"`) do set timePATH=%%i
-for /f "tokens=1,2 delims=." %%a  in ("%timePATH%") do (set timePATH=%%a)
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.player_count_by_team.allied"`) do set alliedcountPATH=%%i
-for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLPATH% ^| %JQ_PATH% -r ".result.player_count_by_team.axis"`) do set axiscountPATH=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.player_count"`) do set countHOT=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.time_remaining"`) do set timeHOT=%%i
+for /f "tokens=1,2 delims=." %%a  in ("%timeHOT%") do (set timeHOT=%%a)
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.player_count_by_team.allied"`) do set alliedcountHOT=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLHOT% ^| %JQ_PATH% -r ".result.player_count_by_team.axis"`) do set axiscountHOT=%%i
 
-if %countPATH% gtr %SEEDED_PATH% (
-    echo Player count is greater than %SEEDED_PATH%.
+if %countHOT% gtr %SEEDED_HOT% (
+    echo Player count is greater than %SEEDED_HOT%.
     goto endloop
 ) else (
-    echo Player count is %countPATH%. Waiting 30 seconds...
-	echo Timeleft: %timePATH%
-	if %timePATH% geq 5280 (
+    echo Player count is %countHOT%. Waiting 30 seconds...
+	echo Timeleft: %timeHOT%
+	if %timeHOT% geq 5280 (
 	echo New Map.
-		if %alliedcountPATH% leq %axiscountPATH% (
+		if %alliedcountHOT% leq %axiscountHOT% (
 		echo Spawning
 		ReSpawnSL.exe Allied
 		) else (
